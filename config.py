986b88040d244ab98e0543c8b161d9ca40a73593
BudarinDm <89217594+BudarinDm@users.noreply.github.com>
2021-10-19 12:43:52 +0300

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-TOKEN = '2042376578:AAEDfOc8hDawaH3TfEEgjqTA55USKoLXlhQ'
-
-url1 = 'https://www.wikihow.com/images_en/thumb/d/db/Get-the-URL-for-Pictures-Step-2-Version-6.jpg/v4-460px-Get-the-URL-for-Pictures-Step-2-Version-6.jpg.webp'
